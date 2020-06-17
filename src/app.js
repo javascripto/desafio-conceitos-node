aec44f77cd33a0a5f59bc0cf054e4242672a41fc
Yuri Almeida <yurialves2@gmail.com>
2020-06-17 01:20:14 -0300

    challenge done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,50 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const id = uuid();
+  const { title, url, techs } = request.body;
+  const repository = { id, title, url, techs, likes: 0 };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repository = repositories.find(repo => repo.id === id);
+  if (!repository) {
+    return response.status(400)
+      .json({ message: 'Repository not found!' });
+  }
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(repo => repo.id === id);
+  if (index === -1) {
+    return response.status(400)
+      .json({ message: 'Repository not found!' });
+  }
+  repositories.splice(index, 1);
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repository = repositories.find(repo => repo.id === id);
+  if (!repository) {
+    return response.status(400).json({ message: 'Repository not found!' });
+  }
+  repository.likes++;
+  return response.json(repository);
 });
 
 module.exports = app;
